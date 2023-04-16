144575798519859b27eaaeb2b0748e6fe1e34043
Ankita-Modak <ankitamodak15@gmail.com>
2023-04-17 02:34:52 +0530

    [Ankita] Number is even or odd

---
+++ BasicFunctionProgram/Even Odd/EvenOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFunctionProgram.Even_Odd
+{
+    internal class EvenOdd
+    {
+        public void evenodd()
+        {
+            Console.WriteLine("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"{num} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is odd.");
+            }
+        }
+    }
+}
--- BasicFunctionProgram/Program.cs
+++ BasicFunctionProgram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BasicFunctionProgram.Even_Odd;
 using BasicFunctionProgram.FC1;
 using BasicFunctionProgram.LY;
 
@@ -11,6 +12,7 @@ namespace BasicFunctionProgram
             Console.WriteLine("Please select an option:");
             Console.WriteLine("1. Flip Coin and print percentage of Heads and Tails");
             Console.WriteLine("2. Check if a year is a Leap Year");
+            Console.WriteLine("3. Check if number is even or odd");
 
             string input = Console.ReadLine();
 
@@ -21,10 +23,17 @@ namespace BasicFunctionProgram
                     Flip flip = new Flip();
                     flip.FlipCoin();
                     break;
+
                 case "2":
                     LeapYear leap = new LeapYear();
                     leap.CheckLeapYear();
                     break;
+
+                case "3":
+                    EvenOdd evenOdd = new EvenOdd();
+                    evenOdd.evenodd();
+                    break;
+
                 default:
                     Console.WriteLine("Invalid input. Please select either 1 or 2.");
                     break;
